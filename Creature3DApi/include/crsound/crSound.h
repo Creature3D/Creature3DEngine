159c2561a322dcec96b38d9ee8b9158a1fa33eea
wucaihua <wucaihua@86aba9cf-fb85-4101-ade8-2f98c1f5b361>
2016-08-30 09:53:30 +0000

    include/crsound

---
+++ Creature3DApi/include/crsound/crSound.h
@@ -0,0 +1,54 @@
+/* Creature3D - Online Game Engine, Copyright (C) 2005 Îâ²Æ»ª(26756325@qq.com)
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+*/
+#ifndef CRSOUND_CRSOUNDSTATE_H
+#define CRSOUND_CRSOUNDSTATE_H
+
+
+#include <CRSound/crExport.h>
+#include <limits>
+#include <string>
+#include <map>
+#include <cassert>
+#include <vector>
+
+#include <CRCore/Referenced.h>
+#include <CRCore/crVector3.h>
+#include <fmod.hpp>
+#include <fmod_errors.h>
+
+
+namespace CRSound {
+
+class CRSOUND_EXPORT crSound : public CRCore::Referenced
+{
+public:
+	crSound();
+	void setName(const std::string& name) { m_name = name; }
+	std::string getName() const { return m_name; }
+	void setFileName(const std::string& file) { m_fileName = file; }
+	std::string getFileName() const { return m_fileName; }
+
+	void createSound(FMOD_MODE mode, FMOD_CREATESOUNDEXINFO *exinfo = NULL);
+	void playSound(FMOD_CHANNELINDEX channelid, bool paused);
+	FMOD::Sound *getSound(){ return m_sound; }
+	FMOD::Channel *getChannel(){ return m_channel; }
+protected:
+	~crSound(); 
+	std::string m_name;
+	std::string m_fileName;
+	FMOD::Sound *m_sound;
+	FMOD::Channel *m_channel;
+};
+
+}
+#endif
---
+++ Creature3DApi/include/crsound/crSoundSystem.h
@@ -0,0 +1,63 @@
+/* Creature3D - Online Game Engine, Copyright (C) 2005 Îâ²Æ»ª(26756325@qq.com)
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+*/
+#ifndef CRSOUND_CRSOUNDSYSTEM_H
+#define CRSOUND_CRSOUNDSYSTEM_H
+
+#include <CRCore/ref_ptr.h>
+#include <CRCore/Referenced.h>
+#include <CRSound/crExport.h>
+#include <CRSound/crSound.h>
+#include <CRCore/thread/crCriticalMutex.h>
+#include <CRCore/crMatrixd.h>
+#include <fmod.hpp>
+#include <fmod_errors.h>
+#include <string>
+#include <map>
+namespace CRSound
+{
+#define FMERRCHECK(result) \
+		if (result != FMOD_OK)\
+		{\
+			printf("FMOD error! (%d) %s\n", result, FMOD_ErrorString(result));\
+		}
+class CRSOUND_EXPORT crSoundSystem : public CRCore::Referenced
+{
+public:
+	static crSoundSystem* getInstance();
+	virtual void clear();
+  
+	void init();
+	void shutdown();
+	crSound *createSound(const std::string& fileName);
+	void addSound(crSound *sound);
+	void insertAmbSound(crSound *sound);
+	crSound* findSound(const std::string &name);
+	FMOD::System *getSystem(){ return m_system; }
+	void update(const CRCore::crMatrixd &viewMat,float dt);
+protected:
+	FMOD::System    *m_system;
+	typedef std::map<std::string, CRCore::ref_ptr<crSound> > SoundMap;
+	SoundMap m_soundMap;
+	CRCore::crCriticalMutex m_soundMutex;
+	typedef std::set<CRCore::ref_ptr<crSound> > AmbSoundSet;
+	AmbSoundSet m_ambSoundSet;
+	crSoundSystem();
+	~crSoundSystem();
+	static CRCore::ref_ptr<crSoundSystem> m_instance;
+	CRCore::crVector3 m_lastpos;
+	float m_ambsoundVolume;
+};
+
+}
+#endif
+
